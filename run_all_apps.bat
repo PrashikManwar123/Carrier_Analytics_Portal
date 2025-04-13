b12ef585bc3db23ab926c9afaa31eb36412116b9
prashikmanwar911 <167643811+PrashikManwar123@users.noreply.github.com>
2025-04-13 10:39:53 +0530

    Update run_all_apps.bat

--- run_all_apps.bat
+++ run_all_apps.bat
@@ -1,3 +1,3 @@
-start cmd /k "streamlit run C:\FP\income_pred\income_prediction_webapp.py --server.port 8501"
-start cmd /k "streamlit run C:\FP\attrition_pred\attriton_prediction_webapp.py --server.port 8502"
-start cmd /k "streamlit run C:\FP\sat_pred\sat_pred_model.py --server.port 8503"
+start cmd /k "streamlit run your-path-to-income_prediction_webapp.py --server.port 8501"
+start cmd /k "streamlit run your-path-to-attriton_prediction_webapp.py --server.port 8502"
+start cmd /k "streamlit run your-path-to-sat_pred_model.py --server.port 8503"
