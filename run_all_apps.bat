f4533e91c15a83b19ae046f3ff8fff7913209723
prashikmanwar911 <167643811+PrashikManwar123@users.noreply.github.com>
2025-04-13 10:03:43 +0530

    Add files via upload

---
+++ run_all_apps.bat
@@ -0,0 +1,3 @@
+start cmd /k "streamlit run C:\FP\income_pred\income_prediction_webapp.py --server.port 8501"
+start cmd /k "streamlit run C:\FP\attrition_pred\attriton_prediction_webapp.py --server.port 8502"
+start cmd /k "streamlit run C:\FP\sat_pred\sat_pred_model.py --server.port 8503"
